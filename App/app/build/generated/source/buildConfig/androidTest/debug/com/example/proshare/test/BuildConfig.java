8a7ff2a9c0633ada640f3283d745d6013fd191af
PANCHUMARTHI ABHINAV <73940343+PanchumarthiAbhinav@users.noreply.github.com>
2022-07-07 11:39:26 +0530

    Add files via upload

---
+++ App/app/build/generated/source/buildConfig/androidTest/debug/com/example/proshare/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.proshare.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.proshare.test";
+  public static final String BUILD_TYPE = "debug";
+}
